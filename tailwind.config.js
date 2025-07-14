f9453a7d2412f3495ca04b1a98ca4ac17a29a9a6
prakhar-2005 <prakharvarshney2005@gmail.com>
2025-07-14 11:51:23 +0530

    navbar bottom glow

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,6 @@ export default {
       },},
       boxShadow: {
         'bottom-blue-glow': '0 6px 15px -3px rgba(59, 130, 246, 0.7)', // Adjust values for desired glow effect (blue-500 with 70% opacity)
-        // You can experiment with different values:
-        // '0 8px 20px -5px rgba(59, 130, 246, 0.6)' for a wider, more diffused glow
       },
   },
   plugins: [],
