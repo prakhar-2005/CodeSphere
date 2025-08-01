ad697ac65502849725d84afb85ea1f881bdd8c55
prakhar-2005 <prakharvarshney2005@gmail.com>
2025-08-01 23:05:19 +0530

    fix: enable cross-site cookie requests

--- backend-server/utils/generateToken.js
+++ backend-server/utils/generateToken.js
@@ -9,8 +9,7 @@ const generateToken = (res, userId) => {
   const cookieOptions = {
     httpOnly: true, 
     secure: process.env.NODE_ENV === 'production' ? true : false,
-    sameSite: 'Lax', // for development
-    // sameSite: 'strict', 
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', 
     maxAge: eval(process.env.JWT_COOKIE_EXPIRE.slice(0, -1)) * 24 * 60 * 60 * 1000, 
     //could use a proper parser for time units
   };
